e9b5063eec8e9cc0a7e822cc60fd5b73cc698b4e
JeonDae_HomePC <james911256@gmail.com>
2023-09-25 21:43:27 +0900

    키 입력 기본 구조 생성

--- main.py
+++ main.py
@@ -8,9 +8,17 @@ character = load_image('2d-fantasy-elf.jpg')
 frame = 0
 
 def handle_events():
-    pass
+    global running, dir #실행, 움직임 방향
+    events = get_events()
+    for event in events: #키 입력 시 입력되는 이벤트
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            pass
+        elif event.type == SDL_KEYUP:
+            pass
 
-while True:
+while running:
     character.clip_draw(frame * 215 + 350, 560, 100, 115, 200, 200)
     # character.clip_draw(left,bottom,width,height,x,y)
     update_canvas()
