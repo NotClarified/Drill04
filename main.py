fc0ddc18dc01c8ebf0adb05a13dfebe2e792bb74
JeonDae_Notebok <104574307+NotClarified@users.noreply.github.com>
2023-09-25 19:52:45 +0900

    이미지 변경

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('move_image.jpg')
+character = load_image('2d-fantasy-elf.jpg')
+
+frame = 0
+
+while True:
+    character.clip_draw(frame * 130 + 230, 560, 300, 115, 200, 200)
+    # character.clip_draw(left,bottom,width,height,x,y)
+    frame = (frame + 1) % 3
+    update_canvas()
+    delay(0.5)
 
 close_canvas()
