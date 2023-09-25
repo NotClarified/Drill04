fedef79d01507feaea3d6dd3c29dc0baadf093a0
JeonDae_Notebok <104574307+NotClarified@users.noreply.github.com>
2023-09-25 20:47:21 +0900

    character 프레임,위치 조절

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ character = load_image('2d-fantasy-elf.jpg')
 frame = 0
 
 while True:
-    character.clip_draw(frame * 130 + 230, 560, 300, 115, 200, 200)
+    character.clip_draw(frame * 215 + 350, 560, 100, 115, 200, 200)
     # character.clip_draw(left,bottom,width,height,x,y)
-    frame = (frame + 1) % 3
+    frame = (frame + 1) % 5
     update_canvas()
     delay(0.5)
 
