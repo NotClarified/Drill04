6dfa77ce387f745c34f014ee0993cb209c80f93b
JeonDae_HomePC <james911256@gmail.com>
2023-09-25 21:37:25 +0900

    handle_events추가

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ character = load_image('2d-fantasy-elf.jpg')
 
 frame = 0
 
+def handle_events():
+    pass
+
 while True:
     character.clip_draw(frame * 215 + 350, 560, 100, 115, 200, 200)
     # character.clip_draw(left,bottom,width,height,x,y)
-    frame = (frame + 1) % 5
     update_canvas()
-    delay(0.5)
+    handle_events()
+    frame = (frame + 1) % 5
+    delay(0.05)
 
 close_canvas()
